19d8eddd4cfe9fae514adf3b158c667a074a9444
Dmitry Tretyakov <dtretyakov@gmail.com>
2024-09-24 23:39:06 +0200

    extract utils

--- src/main/kotlin/Formats.kt
+++
@@ -1,5 +0,0 @@
-package org.jetbrains
-
-enum class ExportFormat {
-    Json
-}
--- src/main/kotlin/SpaceExportCommand.kt
+++ src/main/kotlin/SpaceExportCommand.kt
@@ -1,10 +1,8 @@
 package org.jetbrains
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
-import com.github.ajalt.clikt.parameters.types.enum
 
 class SpaceExportCommand : CliktCommand() {
 
@@ -14,14 +12,9 @@ class SpaceExportCommand : CliktCommand() {
         envvar = "SPACE_ORG"
     ).required()
 
-    private val format by option(
-        help = "Export file format",
-        envvar = "FORMAT",
-    ).enum<ExportFormat>().default(ExportFormat.Json)
-
     override fun run() {
-        currentContext.obj = ExportCommandContext(SpaceApiClient(token, org), format)
+        currentContext.obj = ExportCommandContext(SpaceApiClient(token, org))
     }
 }
 
-class ExportCommandContext(val spaceApiClient: SpaceApiClient, val format: ExportFormat)
+class ExportCommandContext(val spaceApiClient: SpaceApiClient)
