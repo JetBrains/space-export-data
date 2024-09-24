00ce025207433dd87c33c4201dd5b478d75e931c
Dmitry Tretyakov <dtretyakov@gmail.com>
2024-09-24 23:47:22 +0200

    export documents

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@ package org.jetbrains
 
 import com.github.ajalt.clikt.core.subcommands
 import org.jetbrains.chats.ChatsCommand
+import org.jetbrains.documents.DocumentsCommand
 
 fun main(args: Array<String>) = SpaceExportCommand()
     .subcommands(ChatsCommand())
+    .subcommands(DocumentsCommand())
     .main(args)
